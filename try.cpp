8e166de2ca058b64469cf8cf55c4e0452c400742
hiddevuijk <hdvuijk@gmail.com>
2019-06-28 16:34:15 +0200

    start network

---
+++ try.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+class B {
+public:
+    double x, y;
+};
+
+
+
+class A{
+public:
+    B b;
+};
+
+int main()
+{
+
+    return 0;
+}
